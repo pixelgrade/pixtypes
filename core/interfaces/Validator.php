d8240229dd46cb57b0637a323b96bd39cbba3a94
srcspider <source.spider@gmail.com>
2013-09-13 15:10:26 +0300

    updated core to 1.0.2

---
+++ core/interfaces/Validator.php
@@ -0,0 +1,20 @@
+<?php defined('ABSPATH') or die;
+
+/* This file is property of Pixel Grade Media. You may NOT copy, or redistribute
+ * it. Please see the license that came with your copy for more information.
+ */
+
+/**
+ * @package    pixtypes
+ * @category   core
+ * @author     Pixel Grade Team
+ * @copyright  (c) 2013, Pixel Grade Media
+ */
+interface PixtypesValidator {
+
+	/**
+	 * @return array errors
+	 */
+	function validate($input);
+
+} # interface
