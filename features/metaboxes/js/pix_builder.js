3d9fce9a214368f801fe0d0bd5fdc69db12d8446
Razvan Onofrei <razwan.me@gmail.com>
2016-04-01 11:49:37 +0300

    update js markup and event binding

--- features/metaboxes/js/pix_builder.js
+++ features/metaboxes/js/pix_builder.js
@@ -321,15 +321,25 @@
 					'<div class="position__ui-title">Alignment</div>' +
 					'<div class="position__ui-body">' +
 						'<div class="position__ui-row">' +
-							'<div class="position__ui-cell">top</div>' +
+							'<div class="position__ui-cell top">' +
+								'<div class="position__ui-handle">top</div>' +
+							'</div>' +
 						'</div>' +
 						'<div class="position__ui-row">' +
-							'<div class="position__ui-cell">left</div>' +
-							'<div class="position__ui-cell active"></div>' +
-							'<div class="position__ui-cell">right</div>' +
+							'<div class="position__ui-cell left">' +
+								'<div class="position__ui-handle">left</div>' +
+							'</div>' +
+							'<div class="position__ui-cell middle active">' +
+								'<div class="position__ui-handle">middle</div>' +
+							'</div>' +
+							'<div class="position__ui-cell right">' +
+								'<div class="position__ui-handle">right</div>' +
+							'</div>' +
 						'</div>' +
 						'<div class="position__ui-row">' +
-							'<div class="position__ui-cell">button</div>' +
+							'<div class="position__ui-cell bottom">' +
+								'<div class="position__ui-handle">bottom</div>' +
+							'</div>' +
 						'</div>' +
 					'</div>' +
 				'</div>' +
@@ -438,37 +448,34 @@
 		$(".pixbuilder-controls").fixer({gap: 40});
 
 		// margins?
-		$('.position__ui').each(function(i, obj) {
-			var $container = $(obj);
+		$('.pixbuilder-grid').on('click', '.position__ui-cell', function(e) {
+			var $cell = $(this),
+				$container = $cell.closest('.position__ui'),
+				$active = $container.find('.position__ui-cell.active'),
+				$item = $cell.find('.position__ui-handle'),
+				step = $item.attr('data-step'),
+				$target = $active.filter('.middle');
 
-			$container.on('click', '.position__ui-cell', function(e) {
-				var $cell = $(this),
-					$active = $container.find('.position__ui-cell.active'),
-					$item = $cell.find('.position__ui-handle'),
-					step = $item.attr('data-step'),
-					$target = $active.filter('.middle');
+			if ( $cell.is('.middle') ) $target = $active;
+			if ( $cell.is('.top') && $active.filter('.bottom').length ) $target = $active.filter('.bottom');
+			if ( $cell.is('.right') && $active.filter('.left').length ) $target = $active.filter('.left');
+			if ( $cell.is('.bottom') && $active.filter('.top').length ) $target = $active.filter('.top');
+			if ( $cell.is('.left') && $active.filter('.right').length ) $target = $active.filter('.right');
 
-				if ( $cell.is('.middle') ) $target = $active;
-				if ( $cell.is('.top') && $active.filter('.bottom').length ) $target = $active.filter('.bottom');
-				if ( $cell.is('.right') && $active.filter('.left').length ) $target = $active.filter('.left');
-				if ( $cell.is('.bottom') && $active.filter('.top').length ) $target = $active.filter('.top');
-				if ( $cell.is('.left') && $active.filter('.right').length ) $target = $active.filter('.right');
+			$target.removeClass('active');
+			$target.find('.position__ui-handle').attr('data-step', 0);
 
-				$target.removeClass('active');
-				$target.find('.position__ui-handle').attr('data-step', 0);
+			$cell.addClass('active');
 
-				$cell.addClass('active');
+			if ( typeof step === "undefined" ) {
+				step = 1;
+			} else if ( step == 3 ) {
+				step = 0;
+			} else {
+				step = parseInt(step) + 1;
+			}
 
-				if ( typeof step === "undefined" ) {
-					step = 1;
-				} else if ( step == 3 ) {
-					step = 0;
-				} else {
-					step = parseInt(step) + 1;
-				}
-
-				$item.attr('data-step', step);
-			});
+			$item.attr('data-step', step);
 		});
 
 	}); /* Window.load */
