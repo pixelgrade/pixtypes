6d5dd5de1a922fbb065ecbb2fc18e5ad74ef87d7
andreilupu <andrei.lupu@pixelgrade.com>
2016-05-02 15:35:31 +0300

    fix links in pix builder

--- features/metaboxes/js/pix_builder.js
+++ features/metaboxes/js/pix_builder.js
@@ -125,7 +125,6 @@
 			}
 		};
 
-
 		var b64EncodeUnicode = function(str) {
 			return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
 				return String.fromCharCode('0x' + p1);
