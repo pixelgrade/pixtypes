e7436f79390e7868eebb67142da6cc474f2678c9
Cosmin Burloiu <cosmin@pixelgrade.com>
2019-07-17 11:17:31 +0300

    small modification

--- callbacks/save_settings.php
+++ callbacks/save_settings.php
@@ -131,11 +131,6 @@ function save_pixtypes_settings( $values ){
 	// save this settings back
 	update_option('pixtypes_settings', $options);
 
-	/** Usually these settings will change slug settings se we need to flush the permalinks */
-//	global $wp_rewrite;
-//	//Call flush_rules() as a method of the $wp_rewrite object
-//	$wp_rewrite->flush_rules();
-
 	/**
 	 * http://wordpress.stackexchange.com/questions/36152/flush-rewrite-rules-not-working-on-plugin-deactivation-invalid-urls-not-showing
 	 * nothing from above works in plugin so ...
