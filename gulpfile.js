1e8bb1ebe255e2336d211ad801d2775013b6994e
George Olaru <george@pixelgrade.com>
2023-08-22 16:18:12 +0300

    Updated the tasks for Gulp 4.x

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
 /*
  * Load Plugins
  */
-var gulp = require( 'gulp-help' )( require( 'gulp' ) ),
-	exec = require( 'gulp-exec' ),
+var gulp = ( require( 'gulp' ) ),
+    exec = require('gulp-exec'),
     rsync = require('gulp-rsync'),
-    fs = require( 'fs' ),
-    del = require( 'del' );
+    fs = require('fs'),
+    del = require('del');
+
+const { exec: childExec } = require('child_process');
+
 
 /**
  * Copy theme folder outside in a build folder, recreate styles before that
  */
-gulp.task( 'copy-folder', 'Copy plugin production files to a build folder', function() {
-
+function copyFolder(cb) {
     var dir = process.cwd();
-    return gulp.src( './*' )
-        .pipe( exec( 'rm -Rf ./../build; mkdir -p ./../build/pixtypes;', {
+    return gulp.src('./*')
+        .pipe(exec('rm -Rf ./../build; mkdir -p ./../build/pixtypes;', {
             silent: true,
             continueOnError: true // default: false
-        } ) )
+        }))
         .pipe(rsync({
             root: dir,
             destination: '../build/pixtypes/',
@@ -30,15 +32,14 @@ gulp.task( 'copy-folder', 'Copy plugin production files to a build folder', func
             clean: true,
             exclude: ['node_modules']
         }));
-} );
+}
 
 /**
  * Clean the folder of unneeded files and folders
  */
-gulp.task( 'build', 'Remove unneeded files and folders from the build folder', ['copy-folder'], function() {
-
-	// files that should not be present in build zip
-	files_to_remove = [
+function build() {
+    // files that should not be present in build zip
+    files_to_remove = [
 		'**/codekit-config.json',
 		'node_modules',
 		'config.rb',
@@ -78,21 +79,32 @@ gulp.task( 'build', 'Remove unneeded files and folders from the build folder', [
         '.jscsrc',
         '.jshintignore',
         'browserslist'
-	];
+    ];
 
-	files_to_remove.forEach( function( e, k ) {
-		files_to_remove[k] = '../build/pixtypes/' + e;
-	} );
+    files_to_remove.forEach(function(e, k) {
+        files_to_remove[k] = '../build/pixtypes/' + e;
+    });
 
-    return del.sync( files_to_remove, {force: true} );
-} );
+    return del(files_to_remove, { force: true }); // Using the force option here
+}
 
 /**
  * Create a zip archive out of the cleaned folder and delete the folder
  */
-gulp.task( 'zip', 'Create the plugin installer archive and delete the build folder', ['build'], function() {
+function zip(cb) {
+    childExec('cd ./../; rm -rf pixtypes.zip; cd ./build/; zip -r -X ./../pixtypes.zip ./pixtypes; cd ./../; rm -rf build', function(err, stdout, stderr) {
+        if (err) {
+            console.error('Error executing zip command:', err);
+            cb(err);  // Signal completion with an error if there's an issue.
+            return;
+        }
+        console.log(stdout);
+        cb();  // Signal completion when done.
+    });
+}
 
-    return gulp.src( './' )
-        .pipe( exec( 'cd ./../; rm -rf pixtypes.zip; cd ./build/; zip -r -X ./../pixtypes.zip ./pixtypes; cd ./../; rm -rf build' ) );
 
-} );
+// Now, we set up our tasks for Gulp v4:
+gulp.task('copy-folder', copyFolder);
+gulp.task('build', gulp.series(copyFolder, build));
+gulp.task('zip', gulp.series(copyFolder, build, zip));
